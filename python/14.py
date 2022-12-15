dc9b58169b6be4481f40b5dd1481e7187310926d
taksahi <takashi4233@gmail.com>
2022-12-15 15:10:41 +0900

    14 solved

--- python/14.py
+++ python/14.py
@@ -1,6 +1,25 @@
+from typing import List
+
 class Solution:
-    def hogehoge(self, s: str) -> int: 
-        print (s)
+    def longestCommonPrefix(self, strs: List[str]) -> str: 
+        index = 1
+        # リストに１つしか単語が含まれないなら、その文字が答え
+        if len(strs) == 1:
+            return strs[0]
+        # リストの先頭の文字が""なら、こたえは""
+        # 正しくは、リストのどれか一つでもだと思う
+        if len(strs[0]) == 0:
+            return ""
+        # リストの先頭の文字列の１文字目を選ぶ
+        s = strs[0][0]
+        while True:
+            for st in strs:
+                if not st.startswith(s):
+                    return s[0:index-1]
+            index = index + 1
+            if index > len(strs[0]):
+                return s
+            s = strs[0][0:index]
         return s
     
     def test(self,input,answer):
@@ -8,9 +27,36 @@ class Solution:
         
 def main():
     s = Solution()
-    input = "hogehoge"
-    output = 3
-    s.test(s.hogehoge(input),output)       
+    
+    # Prefixで有ることを忘れてた・・・。
+    input = ["c","acc","ccc"]
+    output = ""
+    s.test(s.longestCommonPrefix(input),output)       
+
+    
+    input = ["flower","flower","flower","flower"]
+    output = "flower"
+    s.test(s.longestCommonPrefix(input),output)       
+
+
+    input = ["a"]
+    output = "a"
+    s.test(s.longestCommonPrefix(input),output)       
+
+
+    input = [""]
+    output = ""
+    s.test(s.longestCommonPrefix(input),output)       
+    
+    
+    input = ["flower","flow","flight"]
+    output = "fl"
+    s.test(s.longestCommonPrefix(input),output)       
+
+    input = ["dog","racecar","car"]
+    output = ""
+    s.test(s.longestCommonPrefix(input),output)       
+
 
 if __name__ == "__main__":
     main()
