5e3317d389a5a115661b1ef05d76515cc6ca2235
taksahi <takashi4233@gmail.com>
2023-01-06 08:06:25 +0900

    39 Combination Sum was solved

---
+++ python/39_combinationSum.py
@@ -0,0 +1,52 @@
+from typing import List
+from typing import Optional
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        
+        def comb(i,cur,total):
+            if total == target:
+                res.append(cur.copy())
+                return
+            if i >= len(candidates) or total > target:
+                return
+            cur.append(candidates[i])
+            comb(i,cur,total + candidates[i])
+            cur.pop()
+            comb(i+1,cur,total)
+            
+        comb(0,[],0)
+        return res
+            
+
+    def test(self,input,answer):
+        assert input == answer, '期待する値[{1}], 入力値[{0}]'.format(input, answer)
+
+def main():
+    s = Solution()
+    
+    candidates = [2,3,6,7]
+    target = 7
+    output = [[2,2,3],[7]]
+    ans = s.combinationSum(candidates,target)
+    print (f"ans = {ans}")
+    print (f"out = {output}")
+
+    candidates = [2,3,5]
+    target = 8
+    output = [[2,2,2,2],[2,3,3],[3,5]]
+    ans = s.combinationSum(candidates,target)
+    print (f"ans = {ans}")
+    print (f"out = {output}")
+
+    candidates = [2]
+    target = 1
+    output = []
+    ans = s.combinationSum(candidates,target)
+    print (f"ans = {ans}")
+    print (f"out = {output}")
+
+if __name__ == "__main__":
+    main()
+
