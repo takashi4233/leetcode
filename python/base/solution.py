5fa6f399f65c8792a1e013c235ac21b3046a85c3
taksahi <takashi4233@gmail.com>
2023-02-22 20:31:45 +0900

    テンプレート変更

--- python/base/solution.py
+++ python/base/solution.py
@@ -1,20 +1,22 @@
 from typing import List
 from typing import Optional
 
+
 class Solution:
     def hogehoge(self, s: str) -> int:
-        print (s)
+        print(s)
         return s
 
-    def test(self,input,answer):
-        assert input == answer, '期待する値[{1}], 入力値[{0}]'.format(input, answer)
+    def test(self, input, answer):
+        assert input == answer, "期待する値[{1}], 入力値[{0}]".format(input, answer)
+
 
 def main():
-    s = Solution()
+    sol = Solution()
     input = "hogehoge"
     output = 3
-    s.test(s.hogehoge(input),output)
+    sol.test(sol.hogehoge(input), output)
+
 
 if __name__ == "__main__":
     main()
-
