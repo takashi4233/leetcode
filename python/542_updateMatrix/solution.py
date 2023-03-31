2300326fc6a6acf1f74e975c903b3859fc936b31
taksahi <takashi4233@gmail.com>
2023-03-31 20:55:47 +0900

    solved

--- python/542_updateMatrix/solution.py
+++ python/542_updateMatrix/solution.py
@@ -1,9 +1,38 @@
+import collections
 from typing import List
 from typing import Optional
 
 
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        m = len(mat)
+        n = len(mat[0])
+        dirs = [0, 1, 0, -1, 0]
+        q = collections.deque()
+        seen = [[False] * n for _ in range(m)]
+
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] == 0:
+                    q.append((i, j))
+                    seen[i][j] = True
+
+        while q:
+            i, j = q.popleft()
+            for k in range(4):
+                x = i + dirs[k]
+                y = j + dirs[k + 1]
+                if x < 0 or x == m or y < 0 or y == n:
+                    continue
+                if seen[x][y]:
+                    continue
+                mat[x][y] = mat[i][j] + 1
+                q.append((x, y))
+                seen[x][y] = True
+
+        return mat
+
+    def updateMatrix2(self, mat: List[List[int]]) -> List[List[int]]:
         row, col = len(mat), len(mat[0])
         dd = [[0, 1], [1, 0], [-1, 0], [0, -1]]
         for r in range(row):
